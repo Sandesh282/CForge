170e9ef12aa1114e4656f9cc086c9500bf0d65d3
Sandesh <sandeshac2@gmail.com>
2025-03-29 22:24:44 +0530

    Initial Commit

---
+++ CForge/CForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CForgeApp.swift
+//  CForge
+//
+//  Created by Sandesh Raj on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CForgeTests/CForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CForgeTests.swift
+//  CForgeTests
+//
+//  Created by Sandesh Raj on 29/03/25.
+//
+
+import Testing
+@testable import CForge
+
+struct CForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
